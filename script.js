34082c33ffd9c94cd4814b4ceb399e9d85ab6a9f
ctspencer2 <118775383+ctspencer2@users.noreply.github.com>
2023-11-21 22:15:27 -0800

    return describe
    had some trouble at the end with the apostrophes in the describe return.

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-// Nothing here yet...
-// Make sure to link your JS in index.html!
+
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    describe() {
+        return `This is a ${this.year} ${this.model}, made by ${this.make}.`;
+    }
+}
+
+// Car instances
+const toyotaCamry = new Car("Toyota", "Camry", 2006);
+const toyotaSupra = new Car("Toyota", "Supra", 2005);
+const miniCooper = new Car("Mini", "Cooper", 1969);
+const chevyCamaro = new Car("Chevy", "Camaro", 1969);
+
+// Array of cars
+let allCars = [];
+allCars.push(toyotaCamry, toyotaSupra, miniCooper, chevyCamaro);
+
+// Function to display car information
+function salesMan(car) {
+    console.log(car.describe());
+}
+
+// Loop through all cars and display their information
+for (let car of allCars) {
+    salesMan(car);
+}
